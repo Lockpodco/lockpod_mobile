9ef0949c8a94d370c50bab8832f2b9c5fca71960
Sebastian <sebastiandiaz1222@gmail.com>
2024-04-26 22:14:29 -0700

    Added text input for changing password

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,10 +1,37 @@
 import React from "react";
-import { View, StyleSheet, TouchableOpacity, Text, Pressable, Image, Animated } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text, Pressable, Image, Animated, TextInput } from 'react-native';
 
 const Settings = () => {
+	const [oldPass, oldPassword] = React.useState('');
+	const [newPass, newPassword] = React.useState('');
+	const [retypeNewPass, retypeNewPassword] = React.useState('');
 
     return (
         <View >
+			<View>
+				<Text>Reset your password</Text>
+				<TextInput
+					onChangeText={oldPassword}
+					value={oldPass}
+					placeholder="Old Password"
+					placeholderTextColor={"#808080"}
+					//secureTextEntry={true} uncomment to switch to password dots
+				/>
+				<TextInput
+					onChangeText={newPassword}
+					value={newPass}
+					placeholder="New Password"
+					placeholderTextColor={"#808080"}
+					//secureTextEntry={true}
+				/>
+				<TextInput
+					onChangeText={retypeNewPassword}
+					value={retypeNewPass}
+					placeholder="Retype New Password"
+					placeholderTextColor={"#808080"}
+					//secureTextEntry={true}
+				/>
+			</View>
         </View>
     );
 };

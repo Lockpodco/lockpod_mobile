72e914269dee040b9b892eee9688d87d0f927d07
Sebastian <sebastiandiaz1222@gmail.com>
2024-04-27 12:44:02 -0700

    began implementing stripe

--- screens/Wallet.js
+++ screens/Wallet.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet, TouchableOpacity, Text, Pressable, Image, Animated } from 'react-native';
+import { StripeProvider } from '@stripe/stripe-react-native';
 
-const Wallet = () => {
+const PaymentDisplay = () => {
 
-    return (
-        <View >
-        </View>
-    );
+	return (
+		<StripeProvider
+			publishableKey="pk_live_51P8o6YP66cGixuA1TNpR0SMrCpOviHz2KggvKnIk2TKviFPyOTzFZ1s5LvyKSOBn3bS9GGMKCVrtLJA4ZSCCC1Rt00qVZoUz0F"
+			urlScheme=""
+			merchantIdentifier="merchant.com.{{YOUR_APP_NAME}}"
+		>
+
+		<View>
+
+		</View>
+
+		</StripeProvider>
+	);
 };
 
-export default Wallet;
+export default function Wallet() {
+
+}

beb347271bb2af978351b35a3ee237ef458dcbc9
LillianHo5 <92070679+LillianHo5@users.noreply.github.com>
2024-05-06 14:11:38 -0700

    Set up ScanQR/Unlock screen to take in code input

--- screens/ScanQR.js
+++ screens/ScanQR.js
@@ -1,17 +1,32 @@
-import { Text, View, Button, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  Pressable,
+} from "react-native";
 import { Camera, CameraType } from "expo-camera";
 import { useState } from "react";
 import { BarCodeScanner } from "expo-barcode-scanner";
+import { windowHeight, windowWidth } from "../Constants";
+import { TextInput } from "react-native-gesture-handler";
 
 const ScanQR = () => {
   // MARK: Vars
   const [type, setType] = useState(CameraType.back);
   const [scanned, setScanned] = useState(false);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [code, setCode] = useState("");
 
   const handleBarCodeScanned = ({ data }) => {
     setScanned(true);
     alert(`Barcode scanned with data: ${data}`);
+  };
+
+  const handleSubmit = () => {
+    // Use the value of 'code' when the submit button is pressed
+    alert(`Submitted code: ${code}`);
   };
 
   // MARK: Body
@@ -36,7 +51,7 @@ const ScanQR = () => {
     <View style={styles.container}>
       <Camera
         type={type}
-        style={{ flex: 1 }}
+        style={styles.cameraContainer}
         barCodeScannerSettings={{
           barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr],
         }}
@@ -53,6 +68,16 @@ const ScanQR = () => {
           )}
         </View>
       </Camera>
+      <Text>-or-</Text>
+      <TextInput
+        placeholder="Type in 5-digit code"
+        style={styles.input}
+        value={code}
+        onChangeText={setCode}
+      />
+      <Pressable style={styles.submitButton} onPress={handleSubmit}>
+        <Text style={styles.submitText}>Submit</Text>
+      </Pressable>
     </View>
   );
 };
@@ -61,18 +86,48 @@ const ScanQR = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+  },
+  cameraContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: windowHeight * 0.4,
+    width: windowWidth * 0.4,
+    margin: 50,
   },
   buttonContainer: {
     flex: 1,
   },
   button: {
-    flex: 1,
     justifyContent: "center",
-    alignSelf: "center",
     alignItems: "center",
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
   },
   text: {
     fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+  input: {
+    textAlign: "center",
+    backgroundColor: "white",
+    width: windowWidth * 0.4,
+    padding: 5,
+    marginTop: 20,
+    borderRadius: 10,
+  },
+  submitButton: {
+    backgroundColor: "black",
+    width: windowWidth * 0.4,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginTop: 20,
+  },
+  submitText: {
+    textAlign: "center",
     fontWeight: "bold",
     color: "white",
   },

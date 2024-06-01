ce4c90d13042a88e7a16cb85aff841637fcb0765
Sebastian <sebastiandiaz1222@gmail.com>
2024-06-01 16:37:43 -0700

    added svgs to ProfileScreen

--- screens/MyAccount/ProfileScreen.tsx
+++ screens/MyAccount/ProfileScreen.tsx
@@ -19,63 +19,19 @@ import { Constants } from "../../components/constants";
 import { MediumText, RegularHeading } from "../../components/Text";
 import { DefaultSubmitButton } from "../../components/Buttons";
 
+import ArrowRight from "../../assets/arrowRight.svg"
+import MailIcon from "../../assets/profileIcons/mail.svg"
+import LockIcon from "../../assets/profileIcons/lock.svg"
+import PhoneIcon from "../../assets/profileIcons/phone.svg"
+import BellIcon from "../../assets/profileIcons/bell.svg"
+import LegalIcon from "../../assets/profileIcons/legal.svg"
+
 const ProfileScreen = ({ navigation }: { navigation: any }) => {
 	const [currentPass, setCurrentPass] = React.useState("");
 	const [newPass, setNewPass] = React.useState("");
 	const [confirmNewPass, setConfirmNewPass] = React.useState("");
 
 	const { userProfile, profileDispatch } = useUserProfileContext();
-
-	const [isEnabled, setIsEnabled] = React.useState(false);
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-
-
-	const styles = StyleSheet.create({
-		container: {
-			flex: 1,
-			justifyContent: "space-between",
-		},
-		topContainer: {
-			marginTop: 40,
-			alignItems: "center",
-			justifyContent: "center",
-			gap: 40,
-		},
-		pfp: {
-			height: 100,
-			width: 100,
-		},
-		userDetailsContainer: {
-			marginTop: 70,
-			gap: 18,
-		},
-		userDetails: {
-			flexDirection: "row",
-			justifyContent: "space-between",
-		},
-		icon: {
-			height: 23,
-			alignSelf: "center",
-		},
-		left: {
-			marginLeft: 20,
-			flexDirection: "row",
-			gap: 10,
-			alignItems: "center"
-		},
-		right: {
-			marginRight: 20,
-			flexDirection: "row",
-			alignItems: "center",
-			gap: 20
-		},
-		arrow: {
-			height: 14,
-		},
-		logOutButton: {
-			paddingBottom: Constants.bottomOfPagePadding,
-		},
-	});
 
 	return (
 		<View style={styles.container}>
@@ -91,65 +47,15 @@ const ProfileScreen = ({ navigation }: { navigation: any }) => {
 					/>
 				</View>
 				<View style={styles.userDetailsContainer}>
-					<Pressable style={styles.userDetails}>
-						<View style={styles.left}>
-
-							<MediumText value="Email" style={null} />
-						</View>
-						<View style={styles.right}>
-							<MediumText value="name@gmail.com" style={null} />
-
-						</View>
-					</Pressable>
-					<Pressable
-						style={styles.userDetails}
-						onPress={() => {
-							navigation.navigate("ChangePassword");
-						}}
-					>
-						<View style={styles.left}>
-
-							<MediumText value="Password" style={null} />
-						</View>
-						<View style={styles.right}>
-
-						</View>
-					</Pressable>
-					<Pressable style={styles.userDetails}>
-						<View style={styles.left}>
-
-							<MediumText value="Phone" style={null} />
-						</View>
-						<View style={styles.right}>
-							<MediumText value="000-000-0000" style={null} />
-
-						</View>
-					</Pressable>
-					<View style={styles.userDetails}>
-						<View style={styles.left}>
-
-							<MediumText value="Notifications" style={null} />
-						</View>
-						<View style={styles.right}>
-							<Switch
-								trackColor={{ false: Constants.secondaryDark, true: Constants.lightAccent }}
-								onValueChange={toggleSwitch}
-								value={isEnabled}
-							/>
-						</View>
-					</View>
-					<Pressable style={styles.userDetails}>
-						<View style={styles.left}>
-
-							<MediumText value="Privacy & Legal" style={null} />
-						</View>
-						<View style={styles.right}>
-
-						</View>
-					</Pressable>
+					<UserDetails leftText="Email" rightText="name@gmail.com" LeftComponent={MailIcon} RightComponent={ArrowRight} onPress={null}/>
+					<UserDetails leftText="Password" rightText="" LeftComponent={LockIcon} RightComponent={ArrowRight} 
+						onPress={() => navigation.navigate("ChangePassword")} />
+					<UserDetails leftText="Phone" rightText="000-000-0000" LeftComponent={PhoneIcon} RightComponent={ArrowRight} onPress={null}/>
+					<UserDetails leftText="Notification" rightText="" LeftComponent={BellIcon} RightComponent={NotificationSwitch} onPress={null}/>
+					<UserDetails leftText="Privacy & Legal" rightText="" LeftComponent={LegalIcon} RightComponent={ArrowRight} onPress={null}/>
 				</View>
 			</View>
-			<View style={styles.logOutButton}>
+			<View style={{paddingBottom: Constants.bottomOfPagePadding}}>
 				<DefaultSubmitButton
 					title="Log Out"
 					isActive={true}
@@ -164,5 +70,72 @@ const ProfileScreen = ({ navigation }: { navigation: any }) => {
 		</View>
 	);
 };
+
+function NotificationSwitch() {
+	const [isEnabled, setIsEnabled] = React.useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+	return (
+		<Switch
+			trackColor={{ false: Constants.secondaryDark, true: Constants.lightAccent }}
+			onValueChange={toggleSwitch}
+			value={isEnabled}
+		/>
+		);
+}
+
+function UserDetails({leftText, rightText, LeftComponent, RightComponent, onPress}: 
+	{leftText: string, rightText: string, LeftComponent: any, RightComponent: any, onPress: any}) {
+	return (
+		<View>
+			<Pressable style={styles.userDetails} onPress={onPress}>
+				<View style={styles.left}>
+					<LeftComponent width={24} height={24} />
+					<MediumText value={leftText} style={null} />
+				</View>
+				<View style={styles.right}>
+					<MediumText value={rightText} style={null} />
+					<RightComponent width={24} height={24} />
+				</View>
+			</Pressable>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "space-between",
+	},
+	topContainer: {
+		marginTop: 40,
+		alignItems: "center",
+		justifyContent: "center",
+		gap: 40,
+	},
+	pfp: {
+		height: 100,
+		width: 100,
+	},
+	userDetailsContainer: {
+		marginTop: 70,
+		gap: 18,
+	},
+	userDetails: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+	},
+	left: {
+		marginLeft: 20,
+		flexDirection: "row",
+		gap: 10,
+		alignItems: "center"
+	},
+	right: {
+		marginRight: 20,
+		flexDirection: "row",
+		alignItems: "center",
+		gap: 20
+	},
+});
 
 export default ProfileScreen;
